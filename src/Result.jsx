f8cf87caa2a1135bf2fe18f50da2d8acf9c27242
ashoaa <a.shoaahaghighi@gmail.com>
2023-06-13 16:14:58 +0330

    Clean code

--- src/Result.jsx
+++ src/Result.jsx
@@ -1,26 +1,26 @@
 import { useSelector } from "react-redux";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "./Result.css";
+const players = [1, 2];
 
 const Result = () => {
-  const playre1 = useSelector((state) => state.player1);
+  const player1 = useSelector((state) => state.player1);
   const player2 = useSelector((state) => state.player2);
-
+  const result = players.map((player) => (
+    <div key={player} className="col text-center player-name">
+      Player{" "}
+      <i
+        className={`bi ${
+          player === 1 ? "bi-circle circle" : "bi-x-lg x"
+        } player-icon`}></i>
+      &nbsp;<i className="bi bi-arrow-right"></i>
+      &nbsp;{player === 1 ? player1 : player2}
+    </div>
+  ));
   return (
     <>
       <div id="result-device" className="row col-6 mx-auto h-auto mb-5">
-        <div className="col text-center player-name">
-          Player <i className="bi bi-circle circle player-icon"></i>
-          &nbsp;
-          <i className="bi bi-arrow-right"></i>
-          &nbsp;{playre1}
-        </div>
-        <div className="col text-center player-name">
-          Player <i className="bi bi-x-lg x player-icon"></i>
-          &nbsp;
-          <i className="bi bi-arrow-right"></i>
-          &nbsp;{player2}
-        </div>
+        {result}
       </div>
     </>
   );
