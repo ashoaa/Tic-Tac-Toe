673663b0e1d60cd05acaeae5675c8df7c4f6e3a2
ashoaa <a.shoaahaghighi@gmail.com>
2023-06-23 15:39:28 +0330

    Add reset result

--- src/Reset.jsx
+++ src/Reset.jsx
@@ -3,17 +3,27 @@ import { useDispatch } from "react-redux";
 import { gameActions } from "./store/store.jsx";
 const Reset = () => {
   const dispatch = useDispatch();
-  const clickHandler = () => {
+  const clickGameHandler = () => {
     dispatch(gameActions.resetGame(true));
+  };
+  const clickResultHandler = () => {
+    // localStorage.clear();
+    dispatch(gameActions.resetALL());
   };
   return (
     <>
       <div className="button">
         <button
-          id="reset-btn"
-          className="btn btn-outline-danger px-5"
-          onClick={clickHandler}>
-          Reset
+          id="reset-btn1"
+          className="btn btn-outline-danger px-2"
+          onClick={clickResultHandler}>
+          Reset All
+        </button>
+        <button
+          id="reset-btn2"
+          className="btn btn-outline-primary px-2"
+          onClick={clickGameHandler}>
+          Reset Game
         </button>
       </div>
     </>
